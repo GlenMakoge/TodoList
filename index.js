8a038f6b506f8d2f721ca9b1b141e3866e988306
GlenMakoge <ekundimem@gmail.com>
2022-08-09 23:06:10 +0100

    todo list

---
+++ index.js
@@ -0,0 +1,26 @@
+
+// Variables
+// var age = 21;
+// var firstName = "Glen";
+// var online = true;
+// var name;
+
+// name = "glen";
+// console.log("My age is" ,age);
+// console.log("Hello" ,firstName);
+// console.log("Are you online?:",online);
+// console.log(name)
+
+// var friends = 10;
+// friends=friends+1;
+// console.log(friends);
+
+
+// var myName = window.prompt("Whats Your Name?");
+// console.log("Hello" ,myName)
+
+
+// document.getElementById("myButton").onclick = function(){
+//     var myName = document.getElementById("myText").Value;
+//     console.log("Hello",myName);
+// }
